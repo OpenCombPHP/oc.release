3fa30b99eda79e233be46f78182fa66623dc73af
alee <alee@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-06-23 08:50:40 +0000

    git-svn-id: http://svn.opencomb.com/oc/oc.release/trunk@31 34e84b91-b3e8-45e1-a0bc-4720add8a18f

--- index.php
+++ index.php
@@ -16,14 +16,26 @@ $aPlatform = new Platform(__DIR__) ;
 // 简单配置启动 OC platform,以及扩展, 以后完善
 require 'config.php' ;
 
-// core.user
+
+// coreuser
 $aExtMeta = new ExtensionMetainfo('coreuser','oc\ext\coreuser\CoreUser') ;
+$aExtMeta->load($aPlatform) ;
+
+// developtoolbox
+$aExtMeta = new ExtensionMetainfo('developtoolbox','oc\ext\developtoolbox\DevelopToolbox') ;
 $aExtMeta->load($aPlatform) ;
 
 
 
 // 根据路由设置创建控制器 并 执行
 $aController = $aPlatform->accessRouter()->createRequestController($aPlatform->request()) ;
-$aController->mainRun($aPlatform->request()) ;
+if($aController)
+{
+	$aController->mainRun($aPlatform->request()) ;
+}
+else 
+{
+	header("HTTP/1.0 404 Not Found");
+}
 
 ?>
