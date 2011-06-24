9e6db913c8cda68490dc5f199bcb5837901e0640
alee <alee@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-06-24 01:40:23 +0000

    git-svn-id: http://svn.opencomb.com/oc/oc.release/trunk@32 34e84b91-b3e8-45e1-a0bc-4720add8a18f

--- config.php
+++ config.php
@@ -1,12 +1,15 @@
 <?php 
+use jc\session\OriginalSession;
+use jc\session\Session;
 use jc\db\DB;
 use jc\db\PDODriver;
 
 ini_set('display_errors', 1) ;
 
 // 数据库
-// DB::singleton()->setDriver( new PDODriver("mysql:host=127.0.0.1;dbname=www",'root','1') ) ;
+DB::singleton()->setDriver( new PDODriver("mysql:host=192.168.1.1;dbname=oc",'root','1') ) ;
 
-
+// 会话
+Session::setSingleton( new OriginalSession() ) ;
 
 ?>
