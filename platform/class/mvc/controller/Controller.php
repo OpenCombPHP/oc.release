7d604270ee3b182b862478bb11cf395d04868ce8
alee <alee@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-07-03 08:44:52 +0000

    git-svn-id: http://svn.opencomb.com/oc/oc.release/trunk@91 34e84b91-b3e8-45e1-a0bc-4720add8a18f

--- platform/class/mvc/controller/Controller.php
+++ platform/class/mvc/controller/Controller.php
@@ -1,11 +1,26 @@
 <?php
 namespace oc\mvc\controller ;
 
+
 use jc\mvc\controller\Controller as JcController ;
 
 class Controller extends JcController
 {
-
+	/**
+	 * @return oc\mvc\model\db\Model
+	 */
+    public function createModel($sName,$prototype,array $arrProperties=array(),$bAgg=false,$sClass='jc\\mvc\\model\\db\\Model')
+    {
+    	return parent::createModel($sName,$prototype,$arrProperties,$bAgg,'oc\\mvc\\model\\db\\Model') ;
+    }
+    
+    /** 
+     * @return oc\mvc\view\View
+     */
+    public function createView($sName,$sSourceFile)
+    {
+    	return parent::createView($sName,$sSourceFile,'oc\\mvc\\view\\View') ;
+    }
 }
 
 ?>
