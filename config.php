ac816991c6f5093b1a439d995538d99937650801
alee <alee@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-07-04 01:18:31 +0000

    git-svn-id: http://svn.opencomb.com/oc/oc.release/trunk@96 34e84b91-b3e8-45e1-a0bc-4720add8a18f

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ use jc\db\PDODriver;
 ini_set('display_errors', 1) ;
 
 // 数据库
-DB::singleton()->setDriver( new PDODriver("mysql:host=127.0.0.1;dbname=oc",'root','1') ) ;
+DB::singleton()->setDriver( new PDODriver("mysql:host=wonei-office.3322.org;dbname=oc",'root','1') ) ;
 
 // 会话
 Session::setSingleton( new OriginalSession() ) ;
