71250ad99605e3ee04ebaa187b1da148433df1cb
alee <alee@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-08-01 06:01:36 +0000

    git-svn-id: http://svn.opencomb.com/oc/oc.release/trunk@391 34e84b91-b3e8-45e1-a0bc-4720add8a18f

--- index.php
+++ index.php
@@ -2,13 +2,16 @@
 namespace oc ;
 
 // 初始化 jcat 框架
+use jc\fs\imp\LocalFileSystem;
+use jc\fs\File;
 use oc\mvc\model\db\orm\PAMap;
 use jc\mvc\model\db\orm\PrototypeAssociationMap;
 use oc\ext\ExtensionMetainfo;
-use jc\resrc\htmlresrc\HtmlResourcePoolFactory ;
 
+$t = microtime(1) ;
 
 ini_set('display_errors', 1);
+
 //error_reporting(E_ALL);
 
 require_once __DIR__."/framework/inc.entrance.php" ;
@@ -17,6 +20,8 @@ require_once __DIR__."/platform/class/Platform.php" ;
 require_once __DIR__."/platform/class/system/PlatformFactory.php" ;
 
 $aPlatform = new Platform(__DIR__) ;
+
+print_r($aPlatform->request()->get('xxxxssss')) ;
 
 // 简单配置启动 OC platform,以及扩展, 以后完善
 require 'config.php' ;
@@ -51,11 +56,21 @@ $aPlatform->loadExtension(new ExtensionMetainfo('electronicnewspaper','oc\ext\el
 // bannerad
 $aPlatform->loadExtension(new ExtensionMetainfo('bannerad','oc\ext\bannerad\BannerAd')) ;
 
+// album
+$aPlatform->loadExtension(new ExtensionMetainfo('album','oc\ext\album\Album')) ;
+
 // tourdmedm
 //$aPlatform->loadExtension(new ExtensionMetainfo('tourdmedm','oc\ext\tourdmedm\Tourdmedm')) ;
 
 
+// 启用class编译
+$aPlatform->classLoader()->enableClassCompile() ;
 
+/*
+$aFile = new File(__DIR__.'/extensions/blog/class/Blog.php') ;
+$aPlatform->classLoader()->compiler()->compile($aFile->openReader(),$aPlatform->response()->printer()) ;
+exit() ;
+*/
 
 // 根据路由设置创建控制器 并 执行
 $aController = $aPlatform->accessRouter()->createRequestController($aPlatform->request()) ;
@@ -68,4 +83,5 @@ else
 	header("HTTP/1.0 404 Not Found");
 }
 
+echo microtime(1) - $t ;
 ?>
