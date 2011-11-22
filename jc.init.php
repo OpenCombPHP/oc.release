5eea2c80b3d112106fcecc6d9d844ce2b8191095
alee <zq@alee2002.com>
2011-11-22 19:00:08 +0800

    pdo-mysql 乱码问题

--- jc.init.php
+++ jc.init.php
@@ -24,13 +24,13 @@ $aSetting = $aPlatform->setting() ;
 
 // 数据库
 $sDBConfig = $aSetting->item('/platform/db','config','alpha') ;
-DB::singleton()->setDriver(
-		new PDODriver(
-			$aSetting->item('/platform/db/'.$sDBConfig,'dsn')
-			, $aSetting->item('/platform/db/'.$sDBConfig,'username')
-			, $aSetting->item('/platform/db/'.$sDBConfig,'password')
-		)
+$aDBDriver = new PDODriver(
+		$aSetting->item('/platform/db/'.$sDBConfig,'dsn')
+		, $aSetting->item('/platform/db/'.$sDBConfig,'username')
+		, $aSetting->item('/platform/db/'.$sDBConfig,'password')
+		, $aSetting->item('/platform/db/'.$sDBConfig,'options',array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES 'utf8'"))
 ) ;
+DB::singleton()->setDriver($aDBDriver) ;
 
 // 会话
 Session::setSingleton( new OriginalSession() ) ;
