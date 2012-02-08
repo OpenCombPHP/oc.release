12759d83bbf302a84ed9aef2da5cfdf075a77d01
alee <zq@alee2002.com>
2012-02-08 19:57:39 +0800

    +PlatformShutdowner

---
+++ platform/class/system/PlatformShutdowner.php
@@ -0,0 +1,83 @@
+<?php
+namespace org\opencomb\platform\system ;
+
+use org\opencomb\platform\ext\Extension;
+use org\jecat\framework\fs\FileSystem;
+use org\jecat\framework\lang\Object;
+
+
+/**
+ * @wiki /蜂巢/关闭系统
+ *
+ * org\opencomb\platform\system\PlatformShutdowner::shutdown() 方法关闭系统，并返回后门密钥。
+ * org\opencomb\platform\system\PlatformShutdowner::restore() 方法恢复系统访问。
+ * 
+ * = “后门”密钥 =
+ * 系统在关闭状态时，可以通过 Get/Post/Cookie 等方式，提供”后门“密钥来访问系统。
+ */
+class PlatformShutdowner extends Object
+{
+	public function shutdown($sMessage="系统正在离线升级中……")
+	{		
+		// TODO : 用到了扩展 coresystem 的 setting 内容 ！
+		
+		$sContents = self::$sTemplate ;
+		$sContents = str_replace('%title%', sprintf(Extension::flyweight("coresystem")->setting()->item('/webpage','title-template'),"系统关闭"), $sContents) ;
+		$sContents = str_replace('%keywords%', Extension::flyweight("coresystem")->setting()->item('/webpage','description-template'), $sContents) ;
+		$sContents = str_replace('%description%', Extension::flyweight("coresystem")->setting()->item('/webpage','keywords-template'), $sContents) ;
+		$sContents = str_replace('%contents%', $sMessage, $sContents) ;
+		
+		FileSystem::singleton()->findFile('/lock.shutdown.html',FileSystem::FIND_AUTO_CREATE)->openWriter()->write($sContents) ;
+		
+		return $this->backdoorSecretKey(true) ;
+	}
+	
+	public function backdoorSecretKey($bAutoCreate=false)
+	{
+		if( $aSkFile=FileSystem::singleton()->findFile('/lock.shutdown.backdoor.php') )
+		{
+			return $aSkFile->includeFile(false,false) ;
+		}
+		
+		if($bAutoCreate)
+		{
+			$sBackDoorSecretKey = md5(microtime()) ;
+			
+			FileSystem::singleton()->findFile('/lock.shutdown.backdoor.php',FileSystem::FIND_AUTO_CREATE)->openWriter()->write("<?php
+// 系统关闭的后门密钥，用于管理员进入系统
+return \$sBackDoorSecretKey = '{$sBackDoorSecretKey}' ;") ;
+			
+			return $sBackDoorSecretKey ;
+		}
+		
+		return null ;
+		// is_file(__DIR__.'') or include(__DIR__.'/lock.shutdown.backdoor.php')!=$_REQUEST['shutdown_backdoor_secret_key'] ;
+	}
+	
+	public function restore()
+	{
+		FileSystem::singleton()->delete('/lock.shutdown.html') ;		
+		FileSystem::singleton()->delete('/lock.shutdown.backdoor.php') ;		
+	}
+	
+	static private $sTemplate = <<<TEMPLATE
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		<meta name="keywords" content="%keywords%" />
+		<meta name="description" content="%description%" />
+		<title>%title%</title>
+	</head>
+	
+	<body>
+		%contents%
+	</body>
+</html>
+	
+TEMPLATE
+	;
+}
+
+?>
