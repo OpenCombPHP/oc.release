c899662932764be6a5672e34feb385dfe9a01d76
alee <zq@alee2002.com>
2012-02-17 10:49:13 +0800

    在执行控制器时，增加了ExecuteState 对像

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 namespace org\opencomb\platform ;
 
-$arrInbuildClass = get_declared_classes() ;
-
 // 检查系统关闭锁
+use org\jecat\framework\mvc\controller\ExecuteState;
+
 if( is_file(__DIR__.'/lock.shutdown.html') )
 {
 	// 检查”后门“密钥，方便管理员进入
@@ -34,18 +34,19 @@ $t = microtime(1) ;
 // 简单配置启动 OC platform,以及扩展, 以后完善
 $aPlatform = require 'jc.init.php' ;
 
+
+
 // 根据路由设置创建控制器 并 执行
 $aController = AccessRouter::singleton()->createRequestController(Request::singleton()) ;
 if($aController)
 {
-	$aController->mainRun() ;
+	$aController->mainRun(ExecuteState::flyweight(false,true)) ;
 }
 else 
 {
 	header("HTTP/1.0 404 Not Found");
 	echo "<h1>Page Not Found</h1>" ;
 }
-
 
 if(empty($_REQUEST['rspn'])){
 	//echo $aPlatform->signature() ;
