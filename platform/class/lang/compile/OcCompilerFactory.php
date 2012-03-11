41c106d2797f4b4a07e1fab0ddb4d528ac1bee63
alee <zq@alee2002.com>
2012-03-11 19:30:16 +0800

    完成 OcCompilerFactory

---
+++ platform/class/lang/compile/OcCompilerFactory.php
@@ -0,0 +1,22 @@
+<?php
+namespace org\opencomb\platform\lang\compile ;
+
+use org\jecat\framework\lang\compile\CompilerFactory;
+
+class OcCompilerFactory extends CompilerFactory
+{
+	/**
+	 * return Compiler
+	 */
+	public function create()
+	{
+		$aCompiler = parent::create() ;
+
+		// 编译文件有效性检查的代码生成器
+		$aCompiler->registerGenerator("org\\jecat\\framework\\lang\\compile\\object\\NamespaceDeclare","org\\opencomb\\platform\\lang\\compile\\CompiledValidableCheck") ;
+		
+		return $aCompiler ;
+	}
+}
+
+?>
